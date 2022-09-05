30712558d9e22a9e794570cd5d06387f73b29faa
saurabh <sjkumarmishra.01@gmail.com>
2022-09-05 15:52:28 +0530

    Final commit

---
+++ src/com/marks/StudentMarksIMPL.java
@@ -0,0 +1,11 @@
+package com.marks;
+
+public class StudentMarksIMPL {
+    public static void main(String[] args)
+    {
+        StudentMarks studentMarks = new StudentMarks();
+        System.out.println("MEDICAL STUDENT ="+studentMarks.calculateMarks(24,50,44,67));
+        System.out.println("NON-MEDICAL STUDENT ="+studentMarks.calculateMarks(50,87,59));
+        System.out.println("BUSINESS STUDENT ="+studentMarks.calculateMarks(78.0,89.0,68.0));
+    }
+}
